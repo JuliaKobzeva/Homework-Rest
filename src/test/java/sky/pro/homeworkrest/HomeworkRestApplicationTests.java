a4a62a855b54c3f19ea1e0dc9faf82c0dae61e1b
Юля Кобзева <selemina@mail.ru>
2023-01-09 14:15:14 +0300

    первый коммит

---
+++ src/test/java/sky/pro/homeworkrest/HomeworkRestApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.homeworkrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
